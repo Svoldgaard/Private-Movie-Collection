f1b6442e145c6eb38c0b7a6ebbe91d7a5bb7bb63
Svoldgaard <Svoldgaard96@gmail.com>
2025-01-07 09:02:56 +0100

    BE-Movie & Category

--- src/main/java/dk/easv/privatemoviecollection/BE/Movie.java
+++ src/main/java/dk/easv/privatemoviecollection/BE/Movie.java
@@ -3,9 +3,47 @@ package dk.easv.privatemoviecollection.BE;
 import javafx.scene.control.IndexRange;
 
 public class Movie {
-    public Movie(String text, String text1, String text2) {
+
+    private int id;
+    private String title;
+    private int rating;
+
+    public Movie(int id, String title, int rating) {
+        this.id = id;
+        this.title = title;
+        this.rating = rating;
     }
 
-    public static void deleteMovie(IndexRange selecteMovie) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", rating=" + rating +
+                '}';
     }
 }
