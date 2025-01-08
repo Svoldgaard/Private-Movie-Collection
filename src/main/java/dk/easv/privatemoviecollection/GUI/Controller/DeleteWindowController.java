b587e92379a2e8c8d79e0859f2f7010da04f7542
Kimphung3105 <kimngu01@easv365.dk>
2025-01-08 09:39:47 +0100

    DeleteWindowController

--- src/main/java/dk/easv/privatemoviecollection/GUI/Controller/DeleteWindowController.java
+++ src/main/java/dk/easv/privatemoviecollection/GUI/Controller/DeleteWindowController.java
@@ -1,6 +1,12 @@
 package dk.easv.privatemoviecollection.GUI.Controller;
 
+import dk.easv.privatemoviecollection.MovieMain;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DeleteWindowController {
     public void btnDelete1(ActionEvent actionEvent) {
@@ -9,6 +15,11 @@ public class DeleteWindowController {
     public void btnDelete2(ActionEvent actionEvent) {
     }
 
-    public void btnContinue(ActionEvent actionEvent) {
+    public void btnContinue(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MovieMain.class.getResource("/dk/easv/privatemoviecollection/FXML/DeleteWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 611, 400);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
     }
 }
